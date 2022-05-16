73bf99ee98a693232bf769b99b3b296afe86e066
Ashish SHUKLA <ashish.is@lostca.se>
2022-05-17 00:29:20 +0530

    [automated] Update

--- overlay.nix
+++ overlay.nix
@@ -8,12 +8,12 @@ let
   };
 in {
   firefox-stable = let
-    version = "100.0";
+    version = "100.0.1";
     sources = {
       url =
-        "https://download.cdn.mozilla.net/pub/firefox/releases/100.0/linux-x86_64/en-US/firefox-100.0.tar.bz2";
+        "https://download.cdn.mozilla.net/pub/firefox/releases/100.0.1/linux-x86_64/en-US/firefox-100.0.1.tar.bz2";
       sha512 =
-        "6b44156fcf5674abd826f655e4265ea9867a19566cfe754023e965e028f5d2c1e7222d04fc70d507f98f45d222cccf398b65b64839ae2879430430da82507c9e";
+        "dbf87eb4736978733cb5cb60f2ffe663c568c089239de37bbfefa349ff9ad1d871a7c69eb86a09c73ea95f9476ffd6cb31237899d07eef4fc5e74ca67f597bfd";
     };
   in super.wrapFirefox ((self.firefox-bin-unwrapped.override {
     generated = { inherit version sources; };
@@ -24,7 +24,7 @@ in {
       url =
         "https://download.cdn.mozilla.net/pub/firefox/releases/91.9.0esr/linux-x86_64/en-US/firefox-91.9.0esr.tar.bz2";
       sha512 =
-        "6b44156fcf5674abd826f655e4265ea9867a19566cfe754023e965e028f5d2c1e7222d04fc70d507f98f45d222cccf398b65b64839ae2879430430da82507c9e";
+        "dbf87eb4736978733cb5cb60f2ffe663c568c089239de37bbfefa349ff9ad1d871a7c69eb86a09c73ea95f9476ffd6cb31237899d07eef4fc5e74ca67f597bfd";
     };
   in super.wrapFirefox ((self.firefox-bin-unwrapped.override {
     generated = { inherit version sources; };
