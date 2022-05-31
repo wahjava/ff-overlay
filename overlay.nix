bfe80e5b9ff6283e37f7f97fe5567ba04984bb18
Ashish SHUKLA <ashish.is@lostca.se>
2022-05-31 20:49:16 +0530

    [automated] Update

--- overlay.nix
+++ overlay.nix
@@ -8,34 +8,34 @@ let
   };
 in {
   firefox-stable = let
-    version = "100.0.2";
+    version = "101.0";
     sources = {
       url =
-        "https://download.cdn.mozilla.net/pub/firefox/releases/100.0.2/linux-x86_64/en-US/firefox-100.0.2.tar.bz2";
+        "https://download.cdn.mozilla.net/pub/firefox/releases/101.0/linux-x86_64/en-US/firefox-101.0.tar.bz2";
       sha512 =
-        "5dda3baf3ef4e152a9ef456facf29b5593a429f2cbd281be75081579d5ccbbeb601c1d7dcaa210fbd6f310606c53f0e44749bd0ffa03070ceefecd589cd328f2";
+        "9c43f698d6d42c475479f07f14ba5e190fcb4b999619249846bb1d8cfec902d3e3fde873a5c0df09bea945425d1d6679d3c83188f6130da0a88b5e89014bbf6b";
     };
   in super.wrapFirefox ((self.firefox-bin-unwrapped.override {
     generated = { inherit version sources; };
   }).overrideAttrs (old: { src = super.fetchurl sources; })) common;
   firefox-esr = let
-    version = "91.9.1esr";
+    version = "91.10.0esr";
     sources = {
       url =
-        "https://download.cdn.mozilla.net/pub/firefox/releases/91.9.1esr/linux-x86_64/en-US/firefox-91.9.1esr.tar.bz2";
+        "https://download.cdn.mozilla.net/pub/firefox/releases/91.10.0esr/linux-x86_64/en-US/firefox-91.10.0esr.tar.bz2";
       sha512 =
-        "5dda3baf3ef4e152a9ef456facf29b5593a429f2cbd281be75081579d5ccbbeb601c1d7dcaa210fbd6f310606c53f0e44749bd0ffa03070ceefecd589cd328f2";
+        "9c43f698d6d42c475479f07f14ba5e190fcb4b999619249846bb1d8cfec902d3e3fde873a5c0df09bea945425d1d6679d3c83188f6130da0a88b5e89014bbf6b";
     };
   in super.wrapFirefox ((self.firefox-bin-unwrapped.override {
     generated = { inherit version sources; };
   }).overrideAttrs (old: { src = super.fetchurl sources; })) common;
   firefox-devel = let
-    version = "101.0b9";
+    version = "102.0b1";
     sources = {
       url =
-        "https://download.cdn.mozilla.net/pub/firefox/releases/101.0b9/linux-x86_64/en-US/firefox-101.0b9.tar.bz2";
+        "https://download.cdn.mozilla.net/pub/firefox/releases/102.0b1/linux-x86_64/en-US/firefox-102.0b1.tar.bz2";
       sha512 =
-        "fa4c0566024e22ada00aa47028abd83bf61574d71c4417e058e70ff3e09b5a9529b130905c807356235303c9000c206cfef580a93ffa604a33dfe9bab9567e9d";
+        "a0645a035d9dde9d5e9ecc54bc4f000aaaac2e608cc17a8e4024ec445dc1ebbef8d358f01bcedc18a25078744d7e59145ba41c3c1802f2184930096e6c7997f3";
     };
   in super.wrapFirefox ((self.firefox-bin-unwrapped.override {
     generated = { inherit version sources; };
