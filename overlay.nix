38de065ebfaeb059007f64a89e59fcb8e0d59163
Ashish SHUKLA <ashish.is@lostca.se>
2022-06-09 08:29:30 +0530

    [automated] Update

--- overlay.nix
+++ overlay.nix
@@ -30,12 +30,12 @@ in {
     generated = { inherit version sources; };
   }).overrideAttrs (old: { src = super.fetchurl sources; })) common;
   firefox-devel = let
-    version = "102.0b4";
+    version = "102.0b5";
     sources = {
       url =
-        "https://download.cdn.mozilla.net/pub/firefox/releases/102.0b4/linux-x86_64/en-US/firefox-102.0b4.tar.bz2";
+        "https://download.cdn.mozilla.net/pub/firefox/releases/102.0b5/linux-x86_64/en-US/firefox-102.0b5.tar.bz2";
       sha512 =
-        "84546641e7150ae83c4a03569d1902f31d59ab0ad00a8e29c04e39619d356f99d583a253f28f4d3f3e17abc741977753ba8dad21da410d7f53278b3dbd372269";
+        "24d99cccefdbe36db32555ee53b619f226b3f43b3c349eccea6a3d9e8bc794a5dfa714ba78489e81bf45f0d981f3b448e2bde134e3b45a3b8d245414e3440555";
     };
   in super.wrapFirefox ((self.firefox-bin-unwrapped.override {
     generated = { inherit version sources; };
